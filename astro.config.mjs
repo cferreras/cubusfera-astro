f4db4961e7c916223670ba114abe757dcc0dfca2
Carlos Ferreras <carlos@carlosferreras.com>
2025-07-09 03:25:37 +0200

    feat(config): add server port and host configuration
    Enable server to run on port 3000 and allow host access by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  server:{
+		port: 3000,
+		host: true
+	},
   output: 'server',
   adapter: node({
     mode: 'standalone'
